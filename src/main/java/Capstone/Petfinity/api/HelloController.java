b0a933f3468b84ea16d0cd1169e51f4d454a13f5
yereumi <lovelyyeji01@gmail.com>
2024-04-09 15:58:34 +0900

    mysql 수정

--- src/main/java/Capstone/Petfinity/api/HelloController.java
+++ src/main/java/Capstone/Petfinity/api/HelloController.java
@@ -3,13 +3,15 @@ package Capstone.Petfinity.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class HelloController {
 
     @GetMapping("/hello/yezi")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello";
+    public String hello() {
+//        model.addAttribute("data", "hello!!");
+        return "민식이바보";
     }
 }

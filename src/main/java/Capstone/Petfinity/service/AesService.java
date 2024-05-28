32027d3697835fb46cca85bd1b0187b5c600b0b4
ziyoungkwon <gy011003@naver.com>
2024-05-28 16:51:49 +0900

    aes 암호화 1차

--- src/main/java/Capstone/Petfinity/service/AesService.java
+++ src/main/java/Capstone/Petfinity/service/AesService.java
@@ -6,6 +6,12 @@ import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 //@Service
 //@Transactional(readOnly = true)
 //@RequiredArgsConstructor
@@ -14,3 +20,62 @@ import org.springframework.transaction.annotation.Transactional;
 //
 //    AesBytesEncryptor
 //}
+
+public class AesService {
+
+    public static String algorithms = "AES/CBC/PKCS5Padding";
+
+    private final static String AESKey = "abcdefghabcdefghadcdefghabcdefgh";
+
+    private final static String AESIv = "0123456789abcdef";
+
+    public String encrypt_AES(String ID) {
+
+        try {
+            String result;
+
+            Cipher cipher = Cipher.getInstance(algorithms);
+
+            SecretKeySpec keySpec = new SecretKeySpec(AESKey.getBytes(), "AES");
+
+            IvParameterSpec ivParamSpec = new IvParameterSpec(AESIv.getBytes());
+
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+            byte[] encrypted = cipher.doFinal(ID.getBytes(StandardCharsets.UTF_8));
+            result = Base64.getEncoder().encodeToString(encrypted);
+
+            return result;
+        } catch (Exception e){
+
+            System.out.println("암호화 중 오류 발생");
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    String decypt_AES(String ID) {
+
+        try {
+
+            Cipher cipher = Cipher.getInstance(algorithms);
+
+            SecretKeySpec keySpec = new SecretKeySpec(AESKey.getBytes(), "AES");
+
+            IvParameterSpec ivParamSpec = new IvParameterSpec(AESIv.getBytes());
+
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+            byte[] decodeBytes = Base64.getDecoder().decode(ID);
+            byte[] decrypted = cipher.doFinal(decodeBytes);
+
+            return new String(decrypted, StandardCharsets.UTF_8);
+        } catch (Exception e){
+
+            System.out.println("복호화 중 오류 발생");
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+}
